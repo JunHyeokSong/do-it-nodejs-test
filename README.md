472de22c5f5389254dfab6f15a63411c6568ee4c
song <shinobu2624@unist.ac.kr>
2021-12-21 10:52:18 +0900

    Test simple html sending

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # do-it-nodejs-test
 Repository for testing deploy via heroku 
 
+link: https://do-it-nodejs-test.herokuapp.com/
--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello! World')
+    res.sendFile(__dirname + '/frontend/static/index.html', (err) => {
+        if (err) {
+            console.log(`sendFile of '/' has error: ${err}`)
+            res.end(err.message)
+        }
+    });
 });
+
+app.use(express.static(__dirname + '/frontend/static'))
 
 app.listen(port, ()=>{
     console.log(`Example app listening on port ${port}`)
